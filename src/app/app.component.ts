39010d7a3ccdc8eeaf64b9c8b1023157f8a7a7d4
PdskDev <prestadesk@gmail.com>
2023-03-06 18:59:30 +0100

    formGroup + formControlName

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-
-import { FormControl } from "@angular/forms";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -9,13 +8,24 @@ import { FormControl } from "@angular/forms";
 })
 export class AppComponent implements OnInit {
   title = "angular-reactiveform";
-  firstNameControl!: FormControl;
-  lastNameControl!: FormControl;
+  //firstNameControl!: FormControl;
+  //lastNameControl!: FormControl;
+  myForm!: FormGroup;
 
   ngOnInit(): void {
-    this.firstNameControl = new FormControl();
-    this.firstNameControl.setValue("Nadet");
-    this.lastNameControl = new FormControl();
-    this.lastNameControl.patchValue("Lambi");
+    //this.firstNameControl = new FormControl();
+    //this.firstNameControl.setValue("Nadet");
+    //this.lastNameControl = new FormControl();
+    //this.lastNameControl.patchValue("Lambi");
+
+    /* this.myForm = new FormGroup({
+      firstName: this.firstNameControl,
+      lastName: this.lastNameControl,
+    }); */
+
+    this.myForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+    });
   }
 }
