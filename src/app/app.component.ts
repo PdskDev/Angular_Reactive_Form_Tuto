aa12908a9417555d25603279f3eba7f13919f75a
PdskDev <prestadesk@gmail.com>
2023-03-06 21:49:14 +0100

    FormArray + FormGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormArray, FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -11,21 +11,67 @@ export class AppComponent implements OnInit {
   //firstNameControl!: FormControl;
   //lastNameControl!: FormControl;
   myForm!: FormGroup;
+  colors: any;
+
+  getColors() {
+    return this.myForm.get("colors") as FormArray;
+  }
+
+  addColors(value?: string) {
+    this.colors.push(
+      new FormGroup({
+        name: new FormControl(value),
+      })
+    );
+  }
+
+  submitForm() {
+    console.log(this.myForm.value);
+  }
+
+  removeColor(index: number) {
+    this.colors.removeAt(index);
+  }
 
   ngOnInit(): void {
-    //this.firstNameControl = new FormControl();
-    //this.firstNameControl.setValue("Nadet");
-    //this.lastNameControl = new FormControl();
-    //this.lastNameControl.patchValue("Lambi");
-
-    /* this.myForm = new FormGroup({
-      firstName: this.firstNameControl,
-      lastName: this.lastNameControl,
-    }); */
-
     this.myForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
+      basicInfo: new FormGroup({
+        firstName: new FormControl(),
+        lastName: new FormControl(),
+        email: new FormControl(),
+        age: new FormControl(),
+      }),
+
+      address: new FormGroup({
+        street: new FormControl(),
+        number: new FormControl(),
+        postal: new FormControl(),
+      }),
+
+      /*  colors: new FormArray([
+        new FormGroup({ name: new FormControl("Red") }),
+        new FormGroup({ name: new FormControl("Green") }),
+        new FormGroup({ name: new FormControl("Blue") }),
+      ]), */
     });
+
+    this.myForm.setValue(this.mockData());
+  }
+
+  private mockData() {
+    return {
+      basicInfo: {
+        firstName: "NadetDev",
+        lastName: "Full-Stack",
+        email: "nadetdev@fullstack.dev",
+        age: "25",
+      },
+
+      address: {
+        street: "rue des Développeurs",
+        number: "1",
+        postal: "75000",
+      },
+    };
   }
 }
